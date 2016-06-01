a401d4f722d6a878b8152b48fffb2f2cd05b21fa
Cesar Pino <cpino@twilio.com>
2016-05-31 23:48:23 -0500

    [step-4] Render a list of elements

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,12 @@
 var model = {
-  message: 'The data and the DOM are now linked, and everything is now reactive'
+  list: [
+    {message: 'message 1'},
+    {message: 'message 2'},
+    {message: 'message 3'},
+    {message: 'message 4'},
+    {message: 'message 5'},
+    {message: 'message 6'}
+  ]
 }
 
 new Vue({
