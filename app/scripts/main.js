53de0a57300987af6769dfcb5c7dbe443bfe29fa
Cesar Pino <cpino@twilio.com>
2016-05-31 23:42:35 -0500

    [Step-3] Reactive data explanation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,8 @@
+var model = {
+  message: 'The data and the DOM are now linked, and everything is now reactive'
+}
+
 new Vue({
   el: '#app',
-  data: {
-    message: 'Hi, this is the message var'
-  }
+  data: model
 })
